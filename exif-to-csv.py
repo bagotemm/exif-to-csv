234d3aea90f718b96d603c976a8e825b5340b099
bagotemm <bagot.emmanuel@gmail.com>
2024-08-31 15:49:22 +0200

    Fixes. Add nosubdir option. Sort files and cols in csv.

--- exif-to-csv.py
+++ exif-to-csv.py
@@ -30,6 +30,11 @@ parser.add_argument(
     required=False,
     default=os.getcwd(),
 )
+parser.add_argument(
+    "--nosubdir",
+    help="True if you don't want to scan sub sirdirectories",
+    action="store_true",
+)
 argument = parser.parse_args()
 
 # Set the directory path and CSV file name
@@ -40,7 +45,7 @@ if not (Path.exists(img_path) and Path.is_dir(img_path)):
 print("Dossier parcouru : " + str(img_path))
 csv_path = Path(argument.csv_name)
 # Initialize lists to hold the data
-exif_data_cols = [FILEPATH_COL_NAME]
+exif_data_cols = []
 exif_data = []
 
 
@@ -50,7 +55,7 @@ def get_exif_infos(image_path: str):
     exif_infos = {}
     if is_valid_image_pillow(image_path):
         with Image.open(image_path) as img:
-            if image_path.endswith(".jpg"):
+            if image_path.lower().endswith(".jpg"):
                 exif = img._getexif()
             else:
                 exif = img.getexif()
@@ -71,7 +76,6 @@ def get_exif_infos(image_path: str):
                         if isinstance(gps_element, Iterable):
                             value = list(gps_element)
                             if value:
-                                exif_data_cols.append(decode)
                                 exif_infos[decode] = ".".join(str(x) for x in value)
                 del exif_infos[EXIF_GPS_INFOS_TAG]
             for exif_attr_name in exif_infos.keys():
@@ -80,7 +84,7 @@ def get_exif_infos(image_path: str):
         return exif_infos
 
 
-def is_valid_image_pillow(file_path):
+def is_valid_image_pillow(file_path: str):
     """Function checkung if file is valid image according to pillow lib"""
     try:
         with Image.open(file_path) as img:
@@ -90,38 +94,53 @@ def is_valid_image_pillow(file_path):
         return False
 
 
+def print_error(message_error: str):
+    print(message_error)
+    with open("error.txt", "a") as error_file:
+        error_file.write(message_error)
+        error_file.write(traceback.format_exc())
+
+
 # Delete file if already existing
 Path.unlink(csv_path, missing_ok=True)
 
 # Walk through the directory structure
+i = 1
 for root, dirs, files in os.walk(img_path):
+    files.sort()
     for file in files:
         full_path = os.path.join(root, file)
-        if full_path.endswith((".png", ".jpg", ".bmp")) is False:
+        print("[" + str(i) + "] Analysing " + full_path)
+        if full_path.lower().endswith((".png", ".jpg", ".bmp")) is False:
             continue
         try:
             file_exif_infos = get_exif_infos(full_path)
             if file_exif_infos:
                 exif_data.append({FILEPATH_COL_NAME: full_path} | file_exif_infos)
         except TypeError as te:
-            print("TypeError : KO (" + full_path + ") : " + str(te))
-            print(traceback.format_exc())
+            print_error("TypeError : KO (" + full_path + ") : " + str(te))
         except ValueError as ve:
-            print("ValueError : KO (" + full_path + ") : " + str(ve))
-            print(traceback.format_exc())
+            print_error("ValueError : KO (" + full_path + ") : " + str(ve))
         except KeyboardInterrupt:
             exit()
         except BaseException as be:
-            print("Exception : KO (" + full_path + ") : " + str(be))
-            print(traceback.format_exc())
+            print_error("Exception : KO (" + full_path + ") : " + str(be))
+        i = i + 1
+    if argument.nosubdir:
+        break
 
 
 # Export to CSV
-with open(csv_path, "w", newline="", encoding="utf-8") as csvfile:
-    writer = CsvWriter(
-        csvfile, fieldnames=exif_data_cols, delimiter=";", escapechar="\\"
-    )
-    writer.writeheader()
-    for row in exif_data:
-        writer.writerow(row)
-print("Fichier CSV : " + str(Path.resolve(csv_path)))
+if exif_data:
+    exif_data_cols.sort()
+    exif_data_cols.insert(0, FILEPATH_COL_NAME)
+    with open(csv_path, "w", newline="", encoding="utf-8") as csvfile:
+        writer = CsvWriter(
+            csvfile, fieldnames=exif_data_cols, delimiter=";", escapechar="\\"
+        )
+        writer.writeheader()
+        for row in exif_data:
+            writer.writerow(row)
+    print("Fichier CSV : " + str(Path.resolve(csv_path)))
+else:
+    print("No picture found")
